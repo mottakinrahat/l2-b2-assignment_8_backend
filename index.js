d3ec6bee5afa606bfcd3118f005b9abe63e1a8bf
mottakinrahat <rahatchowhdury661@gmail.com>
2024-03-26 17:47:51 +0600

    first commited

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://next-assignment8-frontend.vercel.app",
     credentials: true,
   })
 );
